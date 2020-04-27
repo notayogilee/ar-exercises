7c10d66cb4bea4065e8dd6311f0cb5cb21234fa1
Lee <notayogilee@gmail.com>
2020-04-27 22:55:11 +0000

    add exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+
+@store1 = Store.where(id: 1)
+@store2 = Store.where(id: 2)
+p @store1
+
+@store1.update(1, name: "Lee's Used Jeans")
+
+p @store1
